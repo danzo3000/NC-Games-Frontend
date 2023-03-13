24b7718f1ac1432f7ceab6bf55fbe948f5db676c
danzo3000 <dbowman9213@gmail.com>
2023-03-13 10:53:19 +0000

    allow cross origin resource sharing

--- nc-games/src/App.js
+++ nc-games/src/App.js
@@ -1,7 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+const cors = require("cors");
 
 function App() {
+  App.use(cors());
   return (
     <div className="App">
       <header className="App-header">
